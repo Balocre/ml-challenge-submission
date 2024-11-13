9ae6e6ddbb1ad9669627901d8564fc25d4e5d645
Antoine Audras <antoineaudras@gmail.com>
2024-11-13 16:29:29 +0100

    feat(utils): add function to calculate size of model

---
+++ part_1/utils.py
@@ -0,0 +1,19 @@
+import torch
+
+def calculate_model_size(model):
+    """ A function to calculate the size of a torch model
+    
+    :param model: The input model
+    
+    :return: The size of the model in bit and MB
+
+    """
+
+    size_model = 0
+    for param in model.parameters():
+        if param.data.is_floating_point():
+            size_model += param.numel() * torch.finfo(param.data.dtype).bits
+        else:
+            size_model += param.numel() * torch.iinfo(param.data.dtype).bits
+    print('woops')
+    return size_model, size_model / 8e6
